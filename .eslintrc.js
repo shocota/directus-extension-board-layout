7648da493dd333efbfd0c84a80b876b37444f94f
shocota <92870471+shocota@users.noreply.github.com>
2023-01-21 12:31:55 +0900

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",  
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {},
+};
